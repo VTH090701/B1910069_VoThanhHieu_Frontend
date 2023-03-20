ff48578e5c2badb22aa829efe903b8c864bc78cf
vothanhhieub1910069 <vothanhhieub1910069>
2023-03-20 23:05:12 +0700

    Tao trang cap nhat lien he

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -29,6 +29,13 @@
                     <i class="fas fa-address-card"></i>
                 </h4>
                 <ContactCard :contact="activeContact" />
+                <router-link :to="{
+                    name: 'contact.edit',
+                    params: { id: activeContact._id },
+                }">
+                    <span class="mt-2 badge badge-warning">
+                        <i class="fas fa-edit"></i> Hiệu chỉnh</span>
+                </router-link>
             </div>
         </div>
     </div>
@@ -43,7 +50,7 @@ export default {
         ContactCard,
         InputSearch,
         ContactList,
-    },  
+    },
     // Đoạn mã xử lý đầy đủ sẽ trình bày bên dưới
     data() {
         return {
